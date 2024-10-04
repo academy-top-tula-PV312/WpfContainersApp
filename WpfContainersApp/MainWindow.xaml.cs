6881c76dfea75392dd57ae7b736d772896faec86
Maxim Efimov <efimov.top.academy@gmail.com>
2024-10-04 21:01:19 +0300

    Добавьте файлы проекта.

---
+++ WpfContainersApp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfContainersApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //for(int i = 0; i < 12; i++)
+            //{
+            //    Button btn = new Button();
+            //    btn.Content = (i + 1).ToString();
+            //    btn.FontSize = 24;
+            //    ugrid.Children.Add(btn);
+            //}
+        }
+    }
+}
